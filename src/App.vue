3476d5c66e9056316b4fd2c68c04be15aaaa95ac
Governess <governesss515@gmail.com>
2023-11-09 22:05:52 -0800

    first attemptat making dynamic grid of each day of the year; having issues with creating unique id for each component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,94 @@
 <template>
   <!-- Passes in the data values into Form.vue, alongside listening for when a new radio button is pressed. -->
-  <Form :question="question" :answers="answers" @update="updatedSelectedAnswer"/>
+  <Form :question="question" :answers="answers" @update="updatedSelectedAnswer" />
+  <Tracker :months="months" :timestamp="timestamp"/>
 </template>
 
 <script>
 import Form from './components/Form.vue'
+import Tracker from './components/Tracker.vue'
 
 export default {
   name: 'App',
   components: {
-    Form
+    Form,
+    Tracker
   },
   data() {
     return {
       question: "What did you accomplish today?",
       answers: ["I put my gym clothes on.", "I went inside the gym.", "I performed light activity (cardio/stretch, < 30 minutes).", "I performed heavy cardio (> 30 minutes).", "I lifted weights.", "I performed cardio AND weights."],
       selectedAnswer: '',
-    }
-  },
+      timestamp: '',
+      months: [
+            {
+              id: "January",
+              days: 31
+            },
+            {
+              id: "February",
+              days: 28
+            },
+            {
+              id: "March",
+              days: 31
+            },
+            {
+              id: "April",
+              days: 30
+            },
+            {
+              id: "May",
+              days: 31
+            },
+            {
+              id: "June",
+              days: 30
+            },
+            {
+              id: "July",
+              days: 31
+            },
+            {
+              id: "August",
+              days: 31
+            },
+            {
+              id: "September",
+              days: 30
+            },
+            {
+              id: "October",
+              days: 31
+            },
+            {
+              id: "November",
+              days: 30
+            },
+            {
+              id: "December",
+              days: 31
+            }
+          ]
+        }
+      },
+  created() {
+      setInterval(this.getNow, 1000);
+    },
   /* 
   When a new radio button is pressed, that value is updated to "selectedAnswer".
   */
   methods: {
     updatedSelectedAnswer(newAnswer){
       this.selectedAnswer = newAnswer
+    },
+    getNow() {
+          const today = new Date();
+          const time = (today.getMonth()+1) + '-' + today.getDate()
+          this.timestamp = time
+        }
+      }
     }
-  }
-}
 </script>
 
 <style>
---
+++ src/components/Tracker.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div v-for="month in months">
+            <div v-for="day in month.days">
+                <component is="div" class="container" :id="makeID">{{ id }}</component>
+                <div>{{ id }}</div>
+<!--                 <div>
+                    {{ month.id }} {{ day }}
+                </div> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "Tracker",
+    props: {
+        months: Array,
+        timestamp: String
+    },
+    methods: {
+        makeID() {
+            console.log(this.month, this.day)
+            return this.month + this.day 
+        }
+    }
+
+}
+</script>
+
+
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(5px, 1fr));
+    gap: .5em;
+    padding: 1em;
+}
+
+</style>
+<!--   <component is="div">This is a DIV element</component>
+ -->
